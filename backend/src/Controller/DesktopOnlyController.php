e7f28e0d861ae259b18386d24e9f12432e5e7488
Anthony Catan-Cavery <a.catancavery@gmail.com>
2026-02-17 11:59:07 +0100

    modification message de redirection, suppression information bloc dashboard admin

--- backend/src/Controller/DesktopOnlyController.php
+++ backend/src/Controller/DesktopOnlyController.php
@@ -14,24 +14,31 @@ class DesktopOnlyController extends AbstractController
     #[Route('/desktop-only', name: 'app_desktop_only', methods: ['GET'])]
     public function index(Request $request): Response
     {
+        // Toutes les fonctionnalités possibles
+        $allFeatures = [
+            [
+                'name' => 'Kanban',
+                'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2',
+                'reason' => 'Drag & drop, organisation complexe de colonnes, édition rapide de tâches'
+            ],
+            [
+                'name' => 'Snippets',
+                'icon' => 'M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4',
+                'reason' => 'Éditeur de code, coloration syntaxique, copier-coller efficace'
+            ],
+        ];
+
+        // Ajouter Dashboard Admin UNIQUEMENT si l'utilisateur est admin
+        if ($this->isGranted('ROLE_ADMIN')) {
+            $allFeatures[] = [
+                'name' => 'Dashboard Admin',
+                'icon' => 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
+                'reason' => 'Visualisation de statistiques globales en grille, métriques multiples nécessitant un grand écran'
+            ];
+        }
+
         return $this->render('desktop_only/index.html.twig', [
-            'desktopFeatures' => [
-                [
-                    'name' => 'Kanban',
-                    'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2',
-                    'reason' => 'Drag & drop, organisation complexe de colonnes, édition rapide de tâches'
-                ],
-                [
-                    'name' => 'Snippets',
-                    'icon' => 'M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4',
-                    'reason' => 'Éditeur de code, coloration syntaxique, copier-coller efficace'
-                ],
-                [
-                    'name' => 'Dashboard Admin',
-                    'icon' => 'M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z',
-                    'reason' => 'Visualisation de statistiques globales en grille, métriques multiples nécessitant un grand écran'
-                ],
-            ]
+            'desktopFeatures' => $allFeatures
         ]);
     }
 }
